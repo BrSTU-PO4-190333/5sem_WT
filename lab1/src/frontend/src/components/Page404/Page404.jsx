666bc9237eb0760c4ac1625de5deca103a8dd75b
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2021-10-16 21:27:44 +0300

    lab1/Galanin_Pavel : Deleted sources

--- lab1/src/frontend/src/components/Page404/Page404.jsx
+++
@@ -1,7 +0,0 @@
-export default function Page404() {
-    return (
-        <div>
-            <h2>Ошибка 404 - страница не найдена</h2>
-        </div>
-    );
-}
--- lab1/src/frontend/src/components/PageHome/PageHome.jsx
+++
@@ -1,9 +0,0 @@
-import { Redirect } from "react-router";
-
-export default function PageHome() {
-    return (
-        <div>
-            <Redirect to="/send-GET" />
-        </div>
-    )
-}
