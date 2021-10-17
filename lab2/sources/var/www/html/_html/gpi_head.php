6e0e067a60a8f624bbec4ab5e9785cb029ff8725
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2021-10-17 16:07:23 +0300

    lab2/Galanin_Pavel : Updated title

--- lab2/sources/var/www/html/_html/gpi_head.php
+++ lab2/sources/var/www/html/_html/gpi_head.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title><? echo $PAGE_TITLE; ?> | ВТ лаб 2</title>
+    <title><? if($PAGE_TITLE) echo $PAGE_TITLE; else echo "Some page"; ?> | ВТ лаб 2</title>
 </head>
 
 <body>
