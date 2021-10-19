dc32cfa17a1fa5ccd35303b0db8fdc0eb99eaa0b
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2021-10-19 03:22:11 +0300

    lab3/Galanin_Pavel : Added API func: delete

--- lab3/src/api/src/gpi_app.js
+++ lab3/src/api/src/gpi_app.js
@@ -14,6 +14,7 @@ app.get("/", function(req, res) {
     res.send("Hello, World!");
 });
 
+/* get */
 app.get("/get", function(req, res) {
     const connection = gpi_connect();
     const sql = "SELECT * FROM `database`.`wt_lab3_data`;";
@@ -26,6 +27,7 @@ app.get("/get", function(req, res) {
     });
 })
 
+/* add */
 app.post("/add", function(req, res) {
     let body = "";
 
@@ -53,6 +55,45 @@ INSERT INTO \`database\`.\`wt_lab3_data\` \
 (\`Name\`, \`Position\`, \`Department\`, \`Phone\`, \`Date\`, \`Email\`, \`Comment\`) \
 VALUES ('${data["Name"]}', '${data["Position"]}', '${data["Department"]}', \
 '${data["Phone"]}', '${data["Date"]}', '${data["Email"]}', "${data["Comment"]}");`;
+        console.log(sql);
+        connection.query(sql, function(error, results, field) {
+            if (error) {
+                connection.end();
+                res.send({
+                    msg: "err",
+                    sql: sql,
+                });
+                console.log(error);
+                return error;
+            }
+            res.send({
+                msg: "success",
+            });
+        });
+    });
+
+});
+
+/* delete */
+app.post("/delete", function(req, res) {
+    let body = "";
+
+    req.on("data", function(chunk) {
+        body += chunk.toString();
+    });
+
+    req.on("end", function() {
+        const gpi_POST = typeof(JSON.parse(body)) == typeof({}) ? JSON.parse(body) : {};
+        if (gpi_POST["ID"] == undefined) {
+            res.send({
+                "msg": `err`,
+                "more": `Error ID: '${gpi_POST["ID"]}'`,
+            });
+            return;
+        }
+
+        const connection = gpi_connect();
+        const sql = `DELETE FROM \`database\`.\`wt_lab3_data\` WHERE \`ID\`='${gpi_POST["ID"]}';`;
         console.log(sql);
         connection.query(sql, function(error, results, field) {
             if (error) {
--- lab3/src/site/src/components/PageViewData/PageViewData.jsx
+++ lab3/src/site/src/components/PageViewData/PageViewData.jsx
@@ -1,22 +1,25 @@
 import { useState } from 'react';
 import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 export default function PageViewData() {
     const [gpi_ConstructorHasRun, gpi_SetConstructorHasRun] = useState(false);
-    const [gpi_ArrayData, gpi_SetArrayData] = useState([{}]);
+    const [gpi_ArrayData, gpi_SetArrayData] = useState([]);
 
     (function gpi_constructor() {
         if (gpi_ConstructorHasRun) return;
         gpi_SetConstructorHasRun(true);
+        gpi_get();
+    }());
 
+    function gpi_get() {
         const URL = `${process.env.REACT_APP__API_URL}:${process.env.REACT_APP__API_PORT}/get`;
         axios.get(URL)
             .then(function (response) {
-                console.log(response);
-                console.log(response.data);
                 if (response?.data?.length) {
                     let arr = response.data;
-                    arr.sort(function(a, b) {
+                    arr.sort(function (a, b) {
                         if (a["ID"] > b["ID"]) return -1;
                         else if (a["ID"] < b["ID"]) return 1;
                         return 0;
@@ -25,11 +28,32 @@ export default function PageViewData() {
                 }
             })
             .catch(function (err) {
-                console.log("Error on PageViewData");
+                console.log("Error on PageViewData '/get'");
                 console.log(err);
             });
+    }
 
-    }());
+    function gpi_delete(id) {
+        const URL = `${process.env.REACT_APP__API_URL}:${process.env.REACT_APP__API_PORT}/delete`;
+        const OBJECT = {
+            ID: id,
+        };
+        axios.post(URL, OBJECT)
+            .then(function (response) {
+                if (response?.data?.msg === "success") {
+                    gpi_get();
+                    alert("Удаление произошло успешно");
+                }
+
+                if (response?.data?.msg === "err") {
+                    alert(response?.data?.more);
+                }
+            })
+            .catch(function (err) {
+                console.log("Error on PageViewData '/delete'");
+                console.log(err);
+            });
+    }
 
     return (
         <div className="container">
@@ -44,6 +68,7 @@ export default function PageViewData() {
                         <td>Дата</td>
                         <td>E-mail</td>
                         <td>Комментарий</td>
+                        <td></td>
                     </tr>
                 </thead>
                 <tbody>
@@ -59,6 +84,14 @@ export default function PageViewData() {
                                     <td>{value.Date}</td>
                                     <td>{value.Email}</td>
                                     <td>{value.Comment}</td>
+                                    <td>
+                                        <button
+                                            className="btn btn-outline-danger"
+                                            onClick={event => gpi_delete(value.ID)}
+                                        >
+                                            <FontAwesomeIcon icon={faTrashAlt} />
+                                        </button>
+                                    </td>
                                 </tr>
                             );
                         })
