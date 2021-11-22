30c1da5870fc0aca6a29d8397118905f7e71a012
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2021-11-23 01:58:11 +0300

    lab5/Galanin_Pavel : Updated task2

--- gpi_wt_lab5/lamp/var/www/html/task2/index.php
+++ gpi_wt_lab5/lamp/var/www/html/task2/index.php
@@ -24,6 +24,10 @@
 ?>
 
 <div class="container">
-    <?php printf("<h2><pre>%-32s %32s</pre></h2>", "=Галанин П. И.=", "=task1="); ?>
-    <?php gpi_mkdir("/var/www/html/task2/someFolder"); ?>
+    <?php printf("<h2><pre>%-32s %32s</pre></h2>", "=Галанин П. И.=", "=task2="); ?>
+    <p><a href="/">На главную страницу</a></p>
+    <?php
+        //gpi_mkdir("/var/www/html/task2/someFolder");
+        gpi_mkdir("someFolder");
+    ?>
 </div>
