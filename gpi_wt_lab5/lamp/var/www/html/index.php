fd19ada26008be839d3c1a934634cf516109956d
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2021-11-23 01:25:50 +0300

    lab5/Galanin_Pavel : Init

---
+++ gpi_wt_lab5/lamp/var/www/html/index.php
@@ -0,0 +1,11 @@
+<?php
+    $gpi_title = "Главная";
+    include "_html/head.html";
+?>
+
+<div class="container">
+    <?php printf("<h2><pre>%-32s %32s</pre></h2>", "=Галанин П. И.=", "=index="); ?>
+    <ul>
+        <li><a href="/task1">1. Написать cfm.-шаблон, выводящий содержимое каталога WWWRoot.</a></li>
+    </ul>
+</div>
