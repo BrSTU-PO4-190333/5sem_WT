0cf581844e09b74cf11b6217a01b89b0c17ff258
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2021-11-23 04:29:11 +0300

    lab5/Galanin_Pavel : Updated print

--- gpi_wt_lab5/lamp/var/www/html/_php/gpi_rm.php
+++ gpi_wt_lab5/lamp/var/www/html/_php/gpi_rm.php
@@ -4,10 +4,10 @@
         $gpi_is_dir = file_exists($gpi_path); // существует ли такой путь?
         if ($gpi_is_dir == 0)
         {
-            echo "rm: cannot remove '$gpi_path': No such file or directory\n";
+            echo "rm: cannot remove '$gpi_path': No such file or directory \n";
             return;
         }
 
         unlink($gpi_path);
-        echo "removed '$gpi_path'\n";
+        echo "removed '$gpi_path' \n";
     }
