bf876956ed01c93cb1156d4802ba5c919ddd0a2b
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2021-11-23 04:22:40 +0300

    lab5/Galanin_Pavel : Updated task3

--- gpi_wt_lab5/lamp/var/www/html/_php/gpi_rename.php
+++ gpi_wt_lab5/lamp/var/www/html/_php/gpi_rename.php
@@ -4,17 +4,10 @@
         $gpi_is_dir = file_exists($gpi_path1); // существует ли такой путь?
         if ($gpi_is_dir == 0)
         {
-            echo "<p style='color: red;'>Каталог\\файл \"$gpi_path1\" не существует! Что переименовывать?</p>";
+            echo "gpi_rename: cannot rename '$gpi_path1': No such file or directory \n";
             return;
         }
 
         $gpi_rename = rename($gpi_path1, $gpi_path2);
-        if ($gpi_rename)
-        {
-            echo "<p style='color: green;'>Каталог\\файл \"$gpi_path1\" на каталог\\файл \"$gpi_path2\" переименован успешно</p>";
-        }
-        else
-        {
-            echo "<p style='color: red;'>Каталог\\файл \"$gpi_path1\" на каталог\\файл \"$gpi_path2\" не переименован!</p>";
-        }
+        echo "gpi_rename: $gpi_path1 => $gpi_path2 \n";
     }
