39b6f5d676b0342ac9e2a99c22f175a524c52260
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2021-11-23 04:27:08 +0300

    lab5/Galanin_Pavel : Updated task9

--- gpi_wt_lab5/lamp/var/www/html/_php/gpi_cat.php
+++ gpi_wt_lab5/lamp/var/www/html/_php/gpi_cat.php
@@ -1,14 +1,14 @@
 <?php
-    function gpi_read_file($gpi_path)
+    function gpi_cat($gpi_path)
     {
         $gpi_is_dir = file_exists($gpi_path); // существует ли такой путь?
         if ($gpi_is_dir == 0)
         {
-            echo "<p style='color: red;'>Файл \"$gpi_path\" не существует! Что читать?</p>";
+            echo "gpi_cat: $gpi_path: No such file or directory \n";
             return;
         }
 
-        $gpi_fd = fopen($gpi_path, 'r') or die("Не удалось открыть файл");
+        $gpi_fd = fopen($gpi_path, 'r');
         while (!feof($gpi_fd))
         {
             $gpi_str = htmlentities(fgets($gpi_fd));
