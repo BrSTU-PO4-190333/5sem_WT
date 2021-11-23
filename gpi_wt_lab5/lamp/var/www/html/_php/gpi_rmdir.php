4abc283ccc35fd3f8ddd44b00f6a9245af9a74ac
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2021-11-23 04:02:42 +0300

    lab5/Galanin_Pavel : Updated task4

---
+++ gpi_wt_lab5/lamp/var/www/html/_php/gpi_rmdir.php
@@ -0,0 +1,25 @@
+<?php
+    function gpi_rmdir($gpi_dir)
+    {
+        $gpi_is_dir = file_exists($gpi_dir); // существует ли такой путь?
+        if ($gpi_is_dir == 0)
+        {
+            echo "gpi_rmdir: failed to remove '$gpi_dir': No such file or directory\n";
+            return;
+        }
+
+        $gpi_files = array_diff(scandir($gpi_dir), array('.','..'));
+        foreach ($gpi_files as $gpi_file) {
+            if (is_dir("$gpi_dir/$gpi_file"))
+            {
+                gpi_rmdir("$gpi_dir/$gpi_file");
+            }
+            else
+            {
+                unlink("$gpi_dir/$gpi_file");
+                echo "removed '$gpi_dir/$gpi_file'\n";
+            }
+        }
+        rmdir($gpi_dir);
+        echo "removed directory '$gpi_dir'\n";
+    }
--- gpi_wt_lab5/lamp/var/www/html/task4/index.php
+++ gpi_wt_lab5/lamp/var/www/html/task4/index.php
@@ -1,11 +1,11 @@
 <?php
     $gpi_title = "Task4";
     include "../_html/head.html";
-    include "../_php/gpi_delete_dir.php";
+    include "../_php/gpi_rmdir.php";
 ?>
 
 <div class="container">
     <?php printf("<h2><pre>%-32s %32s</pre></h2>", "=Галанин П. И.=", "=task4="); ?>
     <p><a href="/">На главную страницу</a></p>
-    <pre><?php gpi_delete_dir("someDir") ?></pre>
+    <pre><?php gpi_rmdir("someDir") ?></pre>
 </div>
