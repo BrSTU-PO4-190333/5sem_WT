96f0bbc4a602506717d87e77454d493cafb861ad
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2021-11-23 03:09:28 +0300

    lab5/Galanin_Pavel : Added task 8

---
+++ gpi_wt_lab5/lamp/var/www/html/task8/index.php
@@ -0,0 +1,13 @@
+<?php
+    $gpi_title = "Task8";
+    include "../_html/head.html";
+    include "../_php/gpi_delete_file.php";
+?>
+
+<div class="container">
+    <?php printf("<h2><pre>%-32s %32s</pre></h2>", "=Галанин П. И.=", "=task8="); ?>
+    <p><a href="/">На главную страницу</a></p>
+    <?php
+        gpi_delete_file("someFile.txt");
+    ?>
+</div>
